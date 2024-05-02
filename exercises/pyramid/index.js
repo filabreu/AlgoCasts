6feee9aa8f6ef9715ea74e26471baa2eeac01649
Filipe Abreu <filabreu@gmail.com>
2024-05-01 21:53:28 -0300

    Resolve pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -10,10 +10,32 @@
 //       ' # '
 //       '###'
 //   pyramid(3)
-//       '  #  '
+//       '  #  ' p.l 0, l.l 0,
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+
+function pyramid(n) {
+  const width = n * 2 - 1;
+  const pyramid = [];
+  let level = '';
+
+  while(pyramid.length < n) {
+    while(level.length < width) {
+      level += level.length < n - pyramid.length - 1 || level.length > n + pyramid.length - 1 ? ' ' : '#';
+    }
+    pyramid.push(level);
+    level = '';
+  }
+
+  for(let levelStr of pyramid) {
+    console.log(levelStr);
+  }
+}
 
 module.exports = pyramid;
