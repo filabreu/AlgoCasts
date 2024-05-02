7eac68e08b9acb9403b1a967e6d35d00285ec2e8
Filipe Abreu <filabreu@gmail.com>
2024-05-01 21:03:30 -0300

    Resolve anagrams exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,29 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let charMapA = {};
+  let charMapB = {};
+
+  for (let char of stringA.replace(/[\W\s]/g, '').toUpperCase()) {
+    charMapA[char] = charMapA[char] ? charMapA[char] + 1 : 1;
+  }
+
+  for (let char of stringB.replace(/[\W\s]/g, '').toUpperCase()) {
+    charMapB[char] = charMapB[char] ? charMapB[char] + 1 : 1;
+  }
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
